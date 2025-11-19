30a752e1b2ab00accaa2d62d40b45beb95e6c6f3
Eligoon <derpepind117@live.dk>
2025-11-19 17:55:13 +0100

    sat up InventoryItem

--- src/InventoryItem.java
+++ src/InventoryItem.java
@@ -1,4 +1,47 @@
 public class InventoryItem
 {
+    private String name;
+    private int price;
+    private int salePrice;
+    private boolean isOnSale;
 
+    public InventoryItem(String name, int price, int salePrice, boolean isOnSale)
+    {
+        this.name = name;
+        this.price = price;
+        this.salePrice = salePrice;
+        this.isOnSale = isOnSale;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(int salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public boolean isOnSale() {
+        return isOnSale;
+    }
+
+    public void setOnSale(boolean onSale) {
+        isOnSale = onSale;
+    }
 }
