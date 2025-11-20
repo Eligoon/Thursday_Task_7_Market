e42570557153daaeefb18d2c037bdc99d21f08d4
Eligoon <derpepind117@live.dk>
2025-11-20 15:19:04 +0100

    Set up basket entry, for items to go into the basket

--- src/BasketEntry.java
+++ src/BasketEntry.java
@@ -1,4 +1,26 @@
-public class BasketEntry
-{
+ public class BasketEntry
+    {
+        private InventoryItem item;
+        private int quantity;
 
-}
+        public BasketEntry(InventoryItem item, int quantity)
+        {
+            this.item = item;
+            this.quantity = quantity;
+        }
+
+        public InventoryItem getItem()
+        {
+            return item;
+        }
+
+        public int getQuantity()
+        {
+            return quantity;
+        }
+
+        public void increaseQuantity(int amount)
+        {
+            this.quantity += amount;
+        }
+    }
