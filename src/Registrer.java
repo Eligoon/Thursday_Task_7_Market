b81c8849b1895c930f1e4c60333a14ee80973074
Eligoon <derpepind117@live.dk>
2025-11-20 15:49:29 +0100

    Class and first method scanBasket set up

--- src/Registrer.java
+++ src/Registrer.java
@@ -1,4 +1,25 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Registrer
 {
+    private Map<String, BasketEntry> entries = new TreeMap<>();
+
+    public void scanBasket(Basket basket)
+    {
+        for (BasketEntry be : basket.getEntries())
+        {
+            String name = be.getItem().getName();
+
+            if (entries.containsKey(name))
+            {
+                entries.get(name).increaseQuantity(be.getQuantity());
+            }
+            else
+            {
+                entries.put(name, be);
+            }
+        }
+    }
 
 }
