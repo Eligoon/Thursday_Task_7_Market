afb701674ea3d7636e46286fbf30232afed0df0c
Eligoon <derpepind117@live.dk>
2025-11-20 15:22:57 +0100

    Basket class to group the basketentries (using treemap to sort them already, knowing it might slow processing a bit)

--- src/Basket.java
+++ src/Basket.java
@@ -1,4 +1,29 @@
+import java.util.Collection;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Basket
 {
+    private Map<String, BasketEntry> entries = new TreeMap<>();
 
+    public void addItem(InventoryItem item)
+    {
+        String name = item.getName();
+
+        if (entries.containsKey(name))
+        {
+            entries.get(name).increaseQuantity(1);
+        }
+        else
+        {
+            entries.put(name, new BasketEntry(item, 1));
+        }
+    }
+
+    public Collection<BasketEntry> getEntries()
+    {
+        return entries.values(); // Always sorted by item name because of Treemapping
+    }
 }
+
+
